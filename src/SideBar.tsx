85477a5bc121759eca4deae1c136fed15566995d
camiloerazo <camiloerazo@javerianacali.edu.co>
2025-02-28 07:42:38 -0500

    Second commit, few changes added

--- src/SideBar.tsx
+++ src/SideBar.tsx
@@ -1,5 +1,13 @@
 function sideBar () {
     console.log("mycomponent ", window);
-    return <p> Hola</p>
+    return (
+        <div id="SideBar">
+            <ul className = "no-bullets">
+                <li>Work experience</li>
+                <li>Education</li>
+                <li>Projets</li>
+            </ul>
+        </div>
+    )
 }
 export  default sideBar
